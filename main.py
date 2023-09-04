055f66b85c680538a968f3d8d84d5d825951c467
Petar Martinović <52103559+Petar47@users.noreply.github.com>
2023-09-04 06:26:33 +0200

    Update and rename glavni.py to main.py

--- main.py
+++ main.py
@@ -1,42 +1,30 @@
 from semanticka import *
 from dnfiknf import *
 from logickaekvivalencija import *
+from sympy import symbols, parse_expr
 from sympy.logic import simplify_logic
+from tabulate import tabulate
 
 izbor=int(input("1. Semantička tablica, DNF i KNF\n2. Logička jednakost sudova\n3. Minimizacija "))
 
-atomi=int(input("Koliko atoma ima sud?:"))
 sud=input("Napisite sud koristeći ~,&,|,>>,<->,() i slova A,B,C... sve odvojeno osim zagrada:")
-
+atomi=len(parse_expr(sud).free_symbols)
 x=[]
 if(izbor==1):
     for i,kombinacija in kombinacije(atomi).items():
         x.append(izrada(expr.parseString(sud)[0],kombinacija))
-    dnff=""
-    knff=""
-    for i in x:
-        """
-        Iterira kroz listu i provjerava disjunktivnu i konjuktivnu normalnu formu 
-        za svaki element.
-    
-        :parametar x: Lista za koje se generiraju DNF i KNF oblici
-        :tip x: list
-        """
-        print(i)
-        if(i[list(i)[-1]]):
-            dnff += dnf(i, atomi) + '|'
-        else:
-            knff += knf(i, atomi) + '&'
-    print("DNF: "+dnff[:-1])
-    print("KNF: "+knff[:-1])
+    kljucevi = list(x[0].keys())
+    tablica = [list(y.values()) for y in x]
+    dnf,knf=dnfiknff(x,atomi)
+    print(tabulate(tablica, headers=kljucevi, tablefmt='grid'))
+    print("DNF: "+dnf)
+    print("KNF: "+knf)
 elif(izbor==2):
-    atomii=int(input("Koliko atoma ima sud?:"))
     sudd=input("Napisite sud koristeći ~,&,|,>>,<->,() i slova A,B,C... sve odvojeno osim zagrada:")
+    atomii=len(parse_expr(sudd).free_symbols)
     if(logickaekvivalencija(sud,atomi,sudd,atomii)):
         print("Sudovi su logicki ekvivalentni")
     else:
         print("Sudovi nisu logicki ekvivalentni")
 elif(izbor==3):
     print(simplify_logic(sud))
-    
-
