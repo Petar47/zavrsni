22f9d88fb4d29563a31ea03333f58687a506b65a
Petar Martinović <52103559+Petar47@users.noreply.github.com>
2023-09-14 19:22:20 +0200

    Update test_izrada.py

--- tests/test_izrada.py
+++ tests/test_izrada.py
@@ -1,4 +1,4 @@
-from ..semanticka import *
+from ..semanticka_tablica import *
 from ..dnfiknf import *
 from ..logickaekvivalencija import *
 import pytest
@@ -14,8 +14,8 @@ def test_dnfiknff(sud, atomi, ocekivani_dnff, ocekivani_knff):
     (['A', '|', 'B'], {'A': False, 'B': False},{'A': False, 'B': False, 'A∨B': False}),  
     (['A', '|', 'B'], {'A': True, 'B': True},{'A': True, 'B': True,'A∨B': True}),  
 ])
-def test_izrada(sud,kombinacija,ocekivani_rezultat):
-    rez = izrada(sud,kombinacija)
+def test_semanticka(sud,kombinacija,ocekivani_rezultat):
+    rez = semanticka(sud,kombinacija)
     assert rez == ocekivani_rezultat
 
     
@@ -23,6 +23,6 @@ def test_izrada(sud,kombinacija,ocekivani_rezultat):
     ("A | B", 2, "B | A", 2, True),  
     ("A & B", 2, "A | B", 2, False),  
 ])
-def test_logickaekvivalencija(sud, atomi, sudd, atomii, ocekivani_rezultat):
+def test_logicka_ekvivalencija(sud, atomi, sudd, atomii, ocekivani_rezultat):
     rezultat = logickaekvivalencija(sud, atomi, sudd, atomii)
     assert rezultat == ocekivani_rezultat
